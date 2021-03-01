538c54b57a7c1219d05c388786f535ca28cb613a
Qingping Hou <dave2008713@gmail.com>
2021-02-28 21:38:43 -0800

    add sql group by query test

--- columnq/src/query/sql.rs
+++ columnq/src/query/sql.rs
@@ -16,3 +16,46 @@ pub async fn exec_query(
 
     df.collect().await.map_err(QueryError::query_exec)
 }
+
+#[cfg(test)]
+mod tests {
+    use arrow::array::*;
+    use datafusion::execution::context::ExecutionContext;
+
+    use super::*;
+    use crate::test_util::*;
+
+    #[tokio::test]
+    async fn group_by_aggregation() -> anyhow::Result<()> {
+        let mut dfctx = ExecutionContext::new();
+        register_table_properties(&mut dfctx)?;
+
+        let batches = exec_query(
+            &dfctx,
+            r#"
+            SELECT DISTINCT(Landlord), COUNT(Address)
+            FROM properties
+            GROUP BY Landlord
+            ORDER BY Landlord
+            "#,
+        )
+        .await?;
+
+        let batch = &batches[0];
+
+        assert_eq!(
+            batch.column(0).as_ref(),
+            Arc::new(StringArray::from(vec![
+                "Carl", "Daniel", "Mike", "Roger", "Sam",
+            ]))
+            .as_ref(),
+        );
+
+        assert_eq!(
+            batch.column(1).as_ref(),
+            Arc::new(UInt64Array::from(vec![3, 3, 4, 3, 2])).as_ref(),
+        );
+
+        Ok(())
+    }
+}
