4644cf798a59f89c997aa1abfc1e6f13e4412f6c
Qingping Hou <dave2008713@gmail.com>
2021-03-07 11:51:00 -0800

    add new table specific schema endpoint

--- roapi-http/src/api/schema.rs
+++ roapi-http/src/api/schema.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 use actix_web::{web, HttpRequest, HttpResponse};
+use serde_derive::Deserialize;
 
 use crate::api::HandlerContext;
 use crate::error::ApiErrResp;
@@ -11,4 +14,26 @@ pub async fn get(
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .body(serde_json::to_vec(data.cq.schema_map()).map_err(ApiErrResp::json_serialization)?))
+}
+
+#[derive(Deserialize)]
+pub struct SchemaTablePath {
+    table_name: String,
+}
+
+pub async fn get_by_table_name(
+    data: web::Data<HandlerContext>,
+    path: web::Path<SchemaTablePath>,
+    _req: HttpRequest,
+    _query: web::Query<HashMap<String, String>>,
+) -> Result<HttpResponse, ApiErrResp> {
+    Ok(HttpResponse::Ok().content_type("application/json").body(
+        serde_json::to_vec(
+            data.cq
+                .schema_map()
+                .get(&path.table_name)
+                .ok_or_else(|| ApiErrResp::not_found("invalid table name"))?,
+        )
+        .map_err(ApiErrResp::json_serialization)?,
+    ))
 }
