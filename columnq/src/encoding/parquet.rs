c79a35a3fc7a254784cef7880b255233e6438275
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2021-08-30 08:45:13 +0200

    add parquet encoding (#64)
    * add parquet encoding
    
    * add alternative content-type for parquet encoding

---
+++ columnq/src/encoding/parquet.rs
@@ -0,0 +1,23 @@
+use datafusion::arrow;
+use datafusion::parquet;
+use datafusion::parquet::errors::ParquetError;
+
+pub fn record_batches_to_bytes(
+    batches: &[arrow::record_batch::RecordBatch],
+) -> Result<Vec<u8>, ParquetError> {
+    let cursor = parquet::util::cursor::InMemoryWriteableCursor::default();
+    {
+        if !batches.is_empty() {
+            let schema = batches[0].schema();
+
+            let mut writer = parquet::arrow::ArrowWriter::try_new(cursor.clone(), schema, None)?;
+            for batch in batches {
+                writer.write(batch)?;
+            }
+            writer.close()?;
+        }
+    }
+
+    let result = cursor.into_inner().expect("Should not fail");
+    Ok(result)
+}
