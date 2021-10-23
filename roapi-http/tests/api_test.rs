03f7afe632396f856c31de4fc4fb87d8b496c07e
Qingping Hou <dave2008713@gmail.com>
2021-10-23 13:55:00 -0700

    add missing test files

---
+++ roapi-http/tests/api_test.rs
@@ -0,0 +1,222 @@
+mod helpers;
+
+use std::collections::HashMap;
+
+use anyhow::Result;
+use columnq::arrow::datatypes::Schema;
+use tokio;
+
+#[actix_rt::test]
+async fn test_schema() -> Result<()> {
+    let json_table = helpers::get_spacex_table();
+    let (app, address) = helpers::test_api_app(vec![json_table]).await;
+    tokio::spawn(app.run_until_stopped());
+
+    let response = helpers::http_get(&format!("{}/api/schema", address)).await;
+
+    assert_eq!(response.status(), 200);
+    let body = response.json::<HashMap<String, Schema>>().await?;
+    assert!(body.contains_key("spacex_launches"));
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn test_uk_cities_sql_post() -> Result<()> {
+    let table = helpers::get_uk_cities_table();
+    let (app, address) = helpers::test_api_app(vec![table]).await;
+    tokio::spawn(app.run_until_stopped());
+
+    let response = helpers::http_post(
+        &format!("{}/api/sql", address),
+        "SELECT city FROM uk_cities WHERE lat > 52 and lat < 53 and lng < -1",
+    )
+    .await;
+
+    assert_eq!(response.status(), 200);
+    let data = response.json::<serde_json::Value>().await?;
+    assert_eq!(
+        data,
+        serde_json::json!([
+            {"city": "Solihull, Birmingham, UK"},
+            {"city": "Rugby, Warwickshire, UK"},
+            {"city": "Sutton Coldfield, West Midlands, UK"},
+            {"city": "Wolverhampton, West Midlands, UK"},
+            {"city": "Frankton, Warwickshire, UK"}
+        ])
+    );
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn test_sql_invalid_post() -> Result<()> {
+    let table = helpers::get_uk_cities_table();
+    let (app, address) = helpers::test_api_app(vec![table]).await;
+    tokio::spawn(app.run_until_stopped());
+
+    let response = helpers::http_post(&format!("{}/api/sql", address), "SELECT city FROM").await;
+
+    assert_eq!(response.status(), 400);
+    let data = response.json::<serde_json::Value>().await?;
+    assert_eq!(
+        data,
+        serde_json::json!({
+            "code": 400,
+            "error": "plan_sql",
+            "message": "Failed to plan execution from SQL query: SQL error: ParserError(\"Expected identifier, found: EOF\")"
+        })
+    );
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn test_ubuntu_ami_sql_post() -> Result<()> {
+    let table = helpers::get_ubuntu_ami_table();
+    let (app, address) = helpers::test_api_app(vec![table]).await;
+    tokio::spawn(app.run_until_stopped());
+
+    let response = helpers::http_post(
+        &format!("{}/api/sql", address),
+        "SELECT ami_id FROM ubuntu_ami \
+                WHERE version='12.04 LTS' \
+                    AND arch = 'amd64' \
+                    AND zone='us-west-2' \
+                    AND instance_type='hvm:ebs-ssd'",
+    )
+    .await;
+
+    assert_eq!(response.status(), 200);
+    let data = response.json::<serde_json::Value>().await?;
+    assert_eq!(
+        data,
+        serde_json::json!([
+            {"ami_id":"<a href=\"https://console.aws.amazon.com/ec2/home?region=us-west-2#launchAmi=ami-270f9747\">ami-270f9747</a>"}
+        ])
+    );
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn test_rest_get() -> Result<()> {
+    let table = helpers::get_ubuntu_ami_table();
+    let (app, address) = helpers::test_api_app(vec![table]).await;
+    tokio::spawn(app.run_until_stopped());
+
+    let response = helpers::http_get(&format!(
+        "{}/api/tables/ubuntu_ami?\
+                columns=name,version,release&\
+                filter[arch]='amd64'&\
+                filter[zone]eq='us-west-2'&\
+                filter[instance_type]eq='hvm:ebs-ssd'&\
+                sort=-version,release\
+                ",
+        address
+    ))
+    .await;
+
+    assert_eq!(response.status(), 200);
+    let data = response.json::<serde_json::Value>().await?;
+    assert_eq!(
+        data,
+        serde_json::json!([
+            { "release": "20201205", "version": "20.10", "name": "groovy" },
+            { "release": "20201201", "version": "20.04 LTS", "name": "focal" },
+            { "release": "20200716.1", "version": "19.10", "name": "eoan" },
+            { "release": "20200115", "version": "19.04", "name": "disco" },
+            { "release": "20201201", "version": "18.04 LTS", "name": "bionic" },
+            { "release": "20201202.1", "version": "16.04 LTS", "name": "xenial" },
+            { "release": "20191107", "version": "14.04 LTS", "name": "trusty" },
+            { "release": "20170502", "version": "12.04 LTS", "name": "precise" }
+        ])
+    );
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn test_graphql_post_query_op() -> Result<()> {
+    let table = helpers::get_ubuntu_ami_table();
+    let (app, address) = helpers::test_api_app(vec![table]).await;
+    tokio::spawn(app.run_until_stopped());
+
+    let response = helpers::http_post(
+        &format!("{}/api/graphql", address),
+        r#"query {
+                    ubuntu_ami(
+                        filter: {
+                            arch: "amd64"
+                            zone: { eq: "us-west-2" }
+                            instance_type: { eq: "hvm:ebs-ssd" }
+                        }
+                        sort: [
+                            { field: "version", order: "desc" }
+                            { field: "release" }
+                        ]
+                    ) {
+                        name
+                        version
+                        release
+                    }
+                }"#,
+    )
+    .await;
+
+    assert_eq!(response.status(), 200);
+    let data = response.json::<serde_json::Value>().await?;
+    assert_eq!(
+        data,
+        serde_json::json!([
+            { "release": "20201205", "version": "20.10", "name": "groovy" },
+            { "release": "20201201", "version": "20.04 LTS", "name": "focal" },
+            { "release": "20200716.1", "version": "19.10", "name": "eoan" },
+            { "release": "20200115", "version": "19.04", "name": "disco" },
+            { "release": "20201201", "version": "18.04 LTS", "name": "bionic" },
+            { "release": "20201202.1", "version": "16.04 LTS", "name": "xenial" },
+            { "release": "20191107", "version": "14.04 LTS", "name": "trusty" },
+            { "release": "20170502", "version": "12.04 LTS", "name": "precise" }
+        ])
+    );
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn test_graphql_post_selection() -> Result<()> {
+    let table = helpers::get_ubuntu_ami_table();
+    let (app, address) = helpers::test_api_app(vec![table]).await;
+    tokio::spawn(app.run_until_stopped());
+
+    let response = helpers::http_post(
+        &format!("{}/api/graphql", address),
+        r#"{
+                ubuntu_ami(
+                    filter: {
+                        arch: "amd64"
+                        zone: { eq: "us-west-2" }
+                        instance_type: { eq: "hvm:ebs-ssd" }
+                    }
+                    sort: [
+                        { field: "version", order: "desc" }
+                    ]
+                ) {
+                    name
+                    version
+                }
+            }"#,
+    )
+    .await;
+
+    assert_eq!(response.status(), 200);
+    let data = response.json::<serde_json::Value>().await?;
+    assert_eq!(
+        data,
+        serde_json::json!([
+            { "version": "20.10", "name": "groovy" },
+            { "version": "20.04 LTS", "name": "focal" },
+            { "version": "19.10", "name": "eoan" },
+            { "version": "19.04", "name": "disco" },
+            { "version": "18.04 LTS", "name": "bionic" },
+            { "version": "16.04 LTS", "name": "xenial" },
+            { "version": "14.04 LTS", "name": "trusty" },
+            { "version": "12.04 LTS", "name": "precise" }
+        ])
+    );
+    Ok(())
+}
---
+++ roapi-http/tests/helpers.rs
@@ -0,0 +1,109 @@
+use std::path::PathBuf;
+
+use columnq::datafusion::arrow;
+use columnq::table::{TableColumn, TableLoadOption, TableSchema, TableSource};
+use roapi_http::config::Config;
+use roapi_http::startup::Application;
+
+pub fn test_data_path(relative_path: &str) -> String {
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("../test_data");
+    d.push(relative_path);
+    d.to_string_lossy().to_string()
+}
+
+pub async fn test_api_app(tables: Vec<TableSource>) -> (Application, String) {
+    let config = Config {
+        addr: "localhost:0".to_string().into(),
+        tables: tables,
+    };
+
+    let app = Application::build(config)
+        .await
+        .expect("Failed to build application config");
+    let port = app.port();
+    let address = format!("http://localhost:{}", port);
+
+    (app, address)
+}
+
+pub async fn http_get(url: &str) -> reqwest::Response {
+    reqwest::Client::new()
+        .get(url)
+        .send()
+        .await
+        .expect("Unable to execute GET request")
+}
+
+pub async fn http_post(url: &str, payload: impl Into<reqwest::Body>) -> reqwest::Response {
+    reqwest::Client::new()
+        .post(url)
+        .body(payload)
+        .send()
+        .await
+        .expect("Unable to execute POST request")
+}
+
+pub fn get_spacex_table() -> TableSource {
+    let json_source_path = test_data_path("spacex_launches.json");
+    TableSource::new("spacex_launches".to_string(), json_source_path)
+}
+
+pub fn get_uk_cities_table() -> TableSource {
+    TableSource::new(
+        "uk_cities".to_string(),
+        test_data_path("uk_cities_with_headers.csv"),
+    )
+}
+
+pub fn get_ubuntu_ami_table() -> TableSource {
+    TableSource::new("ubuntu_ami", test_data_path("ubuntu-ami.json"))
+        .with_option(TableLoadOption::json {
+            pointer: Some("/aaData".to_string()),
+            array_encoded: Some(true),
+        })
+        .with_schema(TableSchema {
+            columns: vec![
+                TableColumn {
+                    name: "zone".to_string(),
+                    data_type: arrow::datatypes::DataType::Utf8,
+                    nullable: true,
+                },
+                TableColumn {
+                    name: "name".to_string(),
+                    data_type: arrow::datatypes::DataType::Utf8,
+                    nullable: true,
+                },
+                TableColumn {
+                    name: "version".to_string(),
+                    data_type: arrow::datatypes::DataType::Utf8,
+                    nullable: true,
+                },
+                TableColumn {
+                    name: "arch".to_string(),
+                    data_type: arrow::datatypes::DataType::Utf8,
+                    nullable: true,
+                },
+                TableColumn {
+                    name: "instance_type".to_string(),
+                    data_type: arrow::datatypes::DataType::Utf8,
+                    nullable: true,
+                },
+                TableColumn {
+                    name: "release".to_string(),
+                    data_type: arrow::datatypes::DataType::Utf8,
+                    nullable: true,
+                },
+                TableColumn {
+                    name: "ami_id".to_string(),
+                    data_type: arrow::datatypes::DataType::Utf8,
+                    nullable: true,
+                },
+                TableColumn {
+                    name: "aki_id".to_string(),
+                    data_type: arrow::datatypes::DataType::Utf8,
+                    nullable: true,
+                },
+            ],
+        })
+}
