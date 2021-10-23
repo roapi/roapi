02120faec414fa58637e8e71284a85c32e6a3f7d
Qingping Hou <dave2008713@gmail.com>
2021-10-23 13:32:04 -0700

    port route tests to integration tests

--- roapi-http/tests/api/helpers.rs
+++
@@ -1,8 +0,0 @@
-use std::path::PathBuf;
-
-pub fn test_data_path(relative_path: &str) -> String {
-    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    d.push("../test_data");
-    d.push(relative_path);
-    d.to_string_lossy().to_string()
-}
