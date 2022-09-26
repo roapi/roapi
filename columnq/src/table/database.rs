4e812b999557eb8090cbaabe58104b323ceb8f80
Qingping Hou <dave2008713@gmail.com>
2022-09-25 18:39:02 -0700

    split database feature flag into database-mysql and database-sqlite

--- columnq/src/table/database.rs
+++ columnq/src/table/database.rs
@@ -4,11 +4,12 @@ pub enum DatabaseLoader {
     Postgres,
 }
 
-#[cfg(feature = "database")]
+#[cfg(any(feature = "database-sqlite", feature = "database-mysql"))]
 mod imp {
     use crate::error::ColumnQError;
     use crate::table::TableSource;
     use connectorx::prelude::*;
+    #[cfg(feature = "database-mysql")]
     use connectorx::sources::mysql::BinaryProtocol;
     use datafusion::arrow::record_batch::RecordBatch;
     use log::debug;
@@ -25,32 +26,51 @@ mod imp {
             let mut destination = ArrowDestination::new();
             match self {
                 DatabaseLoader::MySQL => {
-                    let source = MySQLSource::<BinaryProtocol>::new(t.get_uri_str(), 2)
-                        .map_err(|e| ColumnQError::Database(e.to_string()))?;
-                    let dispatcher =
-                        Dispatcher::<
+                    #[cfg(feature = "database-mysql")]
+                    {
+                        let source = MySQLSource::<BinaryProtocol>::new(t.get_uri_str(), 2)
+                            .map_err(|e| ColumnQError::Database(e.to_string()))?;
+                        let dispatcher = Dispatcher::<
                             MySQLSource<BinaryProtocol>,
                             ArrowDestination,
                             MySQLArrowTransport<BinaryProtocol>,
-                        >::new(source, &mut destination, queries, None);
-                    dispatcher
-                        .run()
-                        .map_err(|e| ColumnQError::Database(e.to_string()))?;
+                        >::new(
+                            source, &mut destination, queries, None
+                        );
+                        dispatcher
+                            .run()
+                            .map_err(|e| ColumnQError::Database(e.to_string()))?;
+                    }
+                    #[cfg(not(feature = "database-mysql"))]
+                    {
+                        return Err(ColumnQError::Database(
+                            "MySQL database feature not enabled.".to_string(),
+                        ));
+                    }
                 }
                 DatabaseLoader::SQLite => {
-                    let uri = t.get_uri_str().replace("sqlite://", "");
-                    let source = SQLiteSource::new(&uri, 2)
-                        .map_err(|e| ColumnQError::Database(e.to_string()))?;
-                    let dispatcher = Dispatcher::<
-                        SQLiteSource,
-                        ArrowDestination,
-                        SQLiteArrowTransport,
-                    >::new(
-                        source, &mut destination, queries, None
-                    );
-                    dispatcher
-                        .run()
-                        .map_err(|e| ColumnQError::Database(e.to_string()))?;
+                    #[cfg(feature = "database-sqlite")]
+                    {
+                        let uri = t.get_uri_str().replace("sqlite://", "");
+                        let source = SQLiteSource::new(&uri, 2)
+                            .map_err(|e| ColumnQError::Database(e.to_string()))?;
+                        let dispatcher = Dispatcher::<
+                            SQLiteSource,
+                            ArrowDestination,
+                            SQLiteArrowTransport,
+                        >::new(
+                            source, &mut destination, queries, None
+                        );
+                        dispatcher
+                            .run()
+                            .map_err(|e| ColumnQError::Database(e.to_string()))?;
+                    }
+                    #[cfg(not(feature = "database-sqlite"))]
+                    {
+                        return Err(ColumnQError::Database(
+                            "SQLite database feature not enabled.".to_string(),
+                        ));
+                    }
                 }
                 DatabaseLoader::Postgres => {
                     // ToDo `Cannot start a runtime from within a runtime` error in `connector-x PostgresSource`
@@ -69,7 +89,7 @@ mod imp {
     }
 }
 
-#[cfg(not(feature = "database"))]
+#[cfg(not(any(feature = "database-sqlite", feature = "database-mysql")))]
 mod imp {
     use crate::error::ColumnQError;
     use crate::table::TableSource;
@@ -90,7 +110,7 @@ mod imp {
 
 pub use imp::*;
 
-#[cfg(feature = "database")]
+#[cfg(any(feature = "database-mysql"))]
 #[cfg(test)]
 mod tests {
     use datafusion::datasource::TableProvider;
@@ -102,6 +122,7 @@ mod tests {
 
     use super::*;
 
+    #[cfg(feature = "database-mysql")]
     #[tokio::test]
     async fn load_mysql() -> anyhow::Result<()> {
         dotenv().ok();
