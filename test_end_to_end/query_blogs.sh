68ee99a92491379c1d90845d9a807b43e1bde79a
Rich <jychen7@users.noreply.github.com>
2023-01-11 01:24:20 -0500

    #18 minio (S3) in CI (#237)
    Full end to end S3 test

---
+++ test_end_to_end/query_blogs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+http_status=$(curl -o /dev/null -s -w "%{http_code}" -X POST -d "SELECT count(1) from blogs" 127.0.0.1:8000/api/sql)
+
+echo $http_status
+if [[ $http_status != "200" ]]
+then
+    echo "error"
+    exit 1
+else
+    echo "success"
+    exit 0
+fi
