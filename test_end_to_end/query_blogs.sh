9fbb4f8ab24b4359134eed000214cf954978dea9
jychen7 <jychen7@users.noreply.github.com>
2023-01-29 22:55:55 -0500

    remove query of space

--- test_end_to_end/query_blogs.sh
+++ test_end_to_end/query_blogs.sh
@@ -10,17 +10,6 @@ else
     echo "success"
 fi
 
-http_status=$(curl -o /dev/null -s -w "%{http_code}" -X POST -d "SELECT count(1) from s3_blogs_space" 127.0.0.1:8000/api/sql)
-
-echo $http_status
-if [[ $http_status != "200" ]]
-then
-    echo "error"
-    exit 1
-else
-    echo "success"
-fi
-
 http_status=$(curl -o /dev/null -s -w "%{http_code}" -X POST -d "SELECT count(1) from s3_blogs_space_encode" 127.0.0.1:8000/api/sql)
 
 echo $http_status
