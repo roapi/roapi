af17f1d27fb69720c0e5c02cc94f34532ebb1f10
Qingping Hou <dave2008713@gmail.com>
2026-03-22 14:07:14 -0700

    ci: pin Azurite server and Azure CLI versions to fix object_store tests

--- ci/scripts/populate_azure.sh
+++ ci/scripts/populate_azure.sh
@@ -5,5 +5,8 @@ set -eux
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TEST_DATA_DIR="${SCRIPT_DIR}/../../test_data"
 
-az storage container create -n test-data --connection-string 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;QueueEndpoint=http://localhost:10001/devstoreaccount1;'
-az storage blob upload -f "${TEST_DATA_DIR}/blogs.parquet" -c test-data -n blogs.parquet --connection-string 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;QueueEndpoint=http://localhost:10001/devstoreaccount1;'
+# Use pinned az cli version via docker to ensure compatibility with Azurite
+AZ_CLI_DOCKER="docker run --rm --network host -v ${TEST_DATA_DIR}:/test_data mcr.microsoft.com/azure-cli:2.60.0 az"
+
+$AZ_CLI_DOCKER storage container create -n test-data --connection-string 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;QueueEndpoint=http://localhost:10001/devstoreaccount1;'
+$AZ_CLI_DOCKER storage blob upload -f "/test_data/blogs.parquet" -c test-data -n blogs.parquet --connection-string 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;QueueEndpoint=http://localhost:10001/devstoreaccount1;'
--- ci/scripts/setup_azure.sh
+++ ci/scripts/setup_azure.sh
@@ -2,4 +2,4 @@
 
 set -eux
 
-docker run -d -p 10000:10000 -p 10001:10001 -p 10002:10002 mcr.microsoft.com/azure-storage/azurite:3.29.0
+docker run -d -p 10000:10000 -p 10001:10001 -p 10002:10002 mcr.microsoft.com/azure-storage/azurite:3.33.0 azurite --skipApiVersionCheck --blobHost 0.0.0.0 --queueHost 0.0.0.0 --tableHost 0.0.0.0
